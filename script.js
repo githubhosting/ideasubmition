d81de049e9a1d8586f2d5562840b9222ac64b853
shravan <94772842+githubhosting@users.noreply.github.com>
2022-04-01 13:26:25 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+//showing the box modal
+function openModal(){
+  const open = document.getElementById("card");
+  open.style.display="flex";
+  open.style.width="300px";
+};
+
+//close the box modal
+function closeModal(){
+  const close = document.getElementById("card");
+  close.style.display="none";
+};
